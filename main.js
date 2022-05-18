79d13b45cd28c3df4ba4db958d44a3f13deb08b3
Thai Tran <thai.tommy.tran@gmail.com>
2022-05-18 14:13:07 -0500

    js interactivity demo completed

--- main.js
+++ main.js
@@ -1,55 +1,45 @@
-console.log("hello world")
+console.log("files are linked");
 
-let count = 0;
+const displayCounter = document.querySelector("#counter");
+const minusBtn = document.querySelector("#minus-btn");
+const plusBtn = document.querySelector("#plus-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
-function increase(){
-    count++
-    counterText.textContent = count
-    console.log(count)
+let counter = 0;
+
+const decrease = () => {
+  counter--;
+  displayCounter.textContent = counter;
+};
+
+const increase = () => {
+  counter++;
+  displayCounter.textContent = counter;
+};
+
+const reset = () => {
+  counter = 0;
+  displayCounter.textContent = counter;
+};
+
+minusBtn.addEventListener("click", decrease);
+plusBtn.addEventListener("click", increase);
+resetBtn.addEventListener("click", reset);
+
+const themeBtns = document.querySelectorAll(".theme-buttons");
+
+const selectTheme = (evt) => {
+  const theme = evt.target.textContent;
+  document.querySelector("body").className = theme;
+  document.querySelector("main").className = theme;
+
+  const allButtons = document.querySelectorAll("button");
+
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].className = theme;
+  }
+};
+
+for (let i = 0; i < themeBtns.length; i++) {
+  themeBtns[i].addEventListener("click", selectTheme);
 }
-
-function decrease(){
-    count--
-    counterText.textContent = count
-    console.log(count)
-}
-
-let reset = () => {
-    count = 0;
-    counterText.textContent = count
-    console.log(count)
-}
-
-const counterText = document.querySelector("#counter")
-
-const minusBtn = document.querySelector("#minus-btn")
-const resetBtn = document.querySelector("#reset-btn")
-const plusBtn = document.querySelector("#plus-btn")
-
-minusBtn.addEventListener('click',decrease)
-resetBtn.addEventListener('click',reset)
-plusBtn.addEventListener('click',increase)
-
-// inline
-// document.querySelector("#minus-btn").addEventListener('click',() => {count--})
-
-const selectTheme = (event) => {
-    const theme = event.target.textContent
-    console.log(theme)
-    document.querySelector('body').className = theme;
-    document.querySelector('main').className = theme;
-
-    const buttons = document.querySelectorAll('button')
-
-    for(let i = 0;i < buttons.length;i++){
-        buttons[i].className = theme
-    }
-}
-
-const themeBtns = document.querySelectorAll('.theme-buttons')
-
-for(let i = 0; i < themeBtns.length;i++){
-    themeBtns[i].addEventListener('click', selectTheme)
-}
-
-
